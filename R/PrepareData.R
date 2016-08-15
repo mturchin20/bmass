7fa21f3702dbeed96144e80ef45b8afc7a9afa14
mturchin20 <mturchin20@gmail.com>
2016-08-14 22:20:45 -0700

    20160814 -- Updating a file

--- R/PrepareData.R
+++ R/PrepareData.R
@@ -15,17 +15,37 @@
 
 Data1 <- read.table("../data/TestData1.txt")
 Data1
-ExpectedColumnNames1 <- c("Chr", "BP", "A1", "MAF", "Direction", "p_Value", "N")
+ExpectedColumnNames <- c("Chr", "BP", "A1", "MAF", "Direction", "p_Value", "N")
 Data2 <- as.matrix(Data1)
 Data2
 DataList <- c("Data1", "Data2")
+#	if (!is.data.frame(eval(parse(text=DataFileName)))) {
 
-
-CheckDataframeFormat <- function (DataFileName, OutputFileBase) {
-	returnValue <- 0
+CheckCharacterFormat <- function (DataSource1) {
+	returnValue <- FALSE
 	
-	if (!is.data.frame(eval(parse(text=DataFileName)))) {
-			returnValue <- 1
+	if (!is.character(DataSource1)) {
+		returnValue <- TRUE
+	}
+
+	return(returnValue)
+}
+
+CheckVariableExists <- function (DataSource1) {
+	returnValue <- FALSE
+	
+	if (!exists(DataSource1)) {
+		returnValue <- TRUE
+	}
+
+	return(returnValue)
+}
+
+CheckDataFrameFormat <- function (DataSource1) {
+	returnValue <- FALSE
+	
+	if (!is.data.frame(DataSource1)) {
+			returnValue <- TRUE
 	}
 
 	return(returnValue)
@@ -114,25 +134,6 @@ CheckDataframeFormat <- function (DataFileName, OutputFileBase) {
 
 #Convert a comma-separated string of data file locations into a vector
 #20160812 CHECK_0 -- Prob: Come up with way to send off log message and then immediately leave program due to an error. Eg, here in this instance, if there are no commas the program should exit -- either the person has inputted things incorrectly or the person has supplied only a single datafile. The former should be obvious and in the latter bmass presumably cannot run on just one file/one phenotype.
-VectorizeDataLocations <- function(DataFileLocations) {
-
-	VectorizedDataFileLocations <- c()
-
-	if (grep(",", DataFileLocations)) {
-
-	}
-	else {
-		log file ( explain error then end with 'exiting...' )
-		exit program? or supply a flag?
-		problem -- at this stage we are trying to create the name for the log file so have not set it up yet
-		use warning() and then exit program? say something like 'cannot initialize function/program'
-		create log file or just create a variable that contains log message output and supply it as one of the output variables via list()? eg output$log
-		can just keep doing rbind() rbind()....start each log message also with some sort of R time call?
-	}
-
-	return(VectorizedDataFileLocations)
-
-}
 
 ~~~
 #> txt3 <- "nana,nana2"
@@ -152,13 +153,6 @@ VectorizeDataLocations <- function(DataFileLocations) {
 #> grep(",", c(txt3,txt3,txt2,txt3))
 #[1] 1 2 4
 ~~~
-
-#20160812 NOTE -- Check/force 'character' setting of input here?
-GetListOfFileNames <- function(DataFileLocations) {
-
-	
-
-}
 
 ~~~
 #> txt1 <- "nana"
@@ -181,18 +175,6 @@ GetListOfFileNames <- function(DataFileLocations) {
 #[1] "nan2"
 ~~~
 
-#I don't recall if I have a greater function for 'OpenData' other than
-OpenData <- function (DataFileNames, DataFileLocations, OutputFileBase) {
-	
-	#Check whether OutputFileBase is NULL and if so, create it from 
-	OutputFileBase <- 
-		
-	#Get list of 	
-
-	return(OutputFileBase)
-
-}
-
 CheckColumns <- function (ExpectedColumnNames, DataFile, OutputFileBase) {
 	DataColumnNames <- names(DataFile)
 	for ColumnName in ExpectedColumnNames {
@@ -202,35 +184,146 @@ CheckColumns <- function (ExpectedColumnNames, DataFile, OutputFileBase) {
 	}
 }
 
+
+
 #This is going to be the main function that goes through each of the steps from beginning to end. Hypothetically, all the other functions presented here should be used through the PrepareData process (or as a subfunction of one of the functions being used in PrepareData)
 #PrepareData <- function (ColumnNames, DataFileNames, OutputFileBase) {
 #PrepareData <- function (ExpectedColumnNames, DataFileNames, DataFileLocations, OutputFileBase) { #20160812 NOTE -- deciding to remove 'DataFileNames' as a requested/given input. Just going to assume that we are given a list of file locations and I will parse that list for the 'names' of the datasources 
-PrepareData <- function (ExpectedColumnNames, DataFileLocations, OutputFileBase) {
+#PrepareData <- function (ExpectedColumnNames, DataFileLocations, OutputFileBase) { #20160814 NOTE -- Changing direction and just assuming input is a single vector that contains all the proper data.frame datasources and working from there. Final output will be a list that has all the output. 'Logfile' will just be a variable included in final list output, developed by continula 'rbind' calls with text output additions. Also deciding to move 'PrepareData' to just a 'MainWorkFlow' or 'Main' that I'll dev in each sub R package and then eventually move to a main source.  
+bmass <- function (DataSources, ExpectedColumnNames=c(c("Chr", "BP", "MAF", "Direction", "p_Value", "N")) {
 
-	#First thing should be setup log file
-	SetUpLogFile <-
+	LogFile1 <- c()
 
-	DataFileLocations <- VectorizeDataLocations(DataFileLocations)
+	LogFile1 <- rbind(LogFile1, paste(format(Sys.time()), " -- beginning bmass."))
 
-	DataFileNames <- GetListOfFilesNames(DataFileLocations)
-
-	OutputFileBase <- OpenData(DataFileNames, DataFileLocations, OutputFileBase) 
-	#If no outputfilebase is provided figure it out from the first input file given?
-
-	DataFrameCheckValues <- apply(DataFileNames, CheckDataframeFormat)
-	if (sum(apply(DataFrameCheckValues, as.numeric)) > 0) {
-		WriteLogFile(OutputFileBase, paste("	
-		cat("
-	
+	#Loading and checking data
+	~~~~~~
+	if (!is.vector(DataSources)) {
+		stop(Sys.time(), " -- input variable DataSources not in vector format. bmass expects DataSources to be a vector of strings. Please fix and rerun bmass.") 
 	}
+	
+	LogFile1 <- rbind(LogFile1, paste(format(Sys.time()), " -- DataSources passed vector check."))
+
+	DataSourcesCheckCharacters <- sapply(DataSources, CheckCharacterFormat)	
+	if (FALSE %in% DataSourcesCheckCharacters) {
+		stop(Sys.time(), " -- the following entries in DataSources were not found as characters. Please fix and rerun bmass: ", DataSources[DataSourcesCheckCharacters])
+	}
+	
+	LogFile1 <- rbind(LogFile1, paste(format(Sys.time()), " -- DataSources passed string check."))
+
+	DataSourcesCheckExists <- sapply(DataSources, CheckVariableExists)
+	if (FALSE %in% DataSourcesCheckExists) {
+		stop(Sys.time(), " -- the variables associated with the following entries in DataSources were not found to exist. Please fix and rerun bmass: ", DataSources[DataSourcesCheckExists])
+	}
+
+	LogFile1 <- rbind(LogFile1, paste(format(Sys.time()), " -- DataSources passed exists check."))
+	
+	eval(parse(text=DataFileName))
+
+	#20160814 20160814 CHECK_1 -- Prob: Create way to specify which files are causing the data.frame failure here? Maybe change DataFrameCheckValues into a vector of true/false statements and then convert the trues to their text names as posible outputs? Soln: Moved to a format where returning TRUE and FALSE statements in a vector, and then pass that vector to DataSources character vector to get proper output. 
+	DataSourcesCheckDataFrames <- sapply(DataSources, CheckDataFrameFormat)
+	if (FALSE %in% DataSourcesCheckDataFrames) {
+		stop(Sys.time(), " -- the following data sources are not formatted as data.frames. Please fix and rerun bmass: ", DataSources[DataSourcesCheckDataFrames])
+	}
+
+	LogFile1 <- rbind(LogFile1, paste(format(Sys.time()), " -- DataSources passed data.frame check."))
+
+	#Check DataSources headers for proper names
+
+	
+
+	#Preparing and merging data
+	~~~~~~
+
+
 
 }
 
+#Expectation
+#Vector input of data.frame data sources 
 
-
-
+#' @param DataSources input names of datasources
+#' @param LogFile1 rbind'ing vector text output
+#' @param ExpectedColumnNames Comma-separ
 
 #' @param DataFileNames Comma-separated list of all the phenotypes (ie names associated with each datafile) being analyzed. This should be in the same order as DataFileLocations.
 #' @param DataFileLocations Comma-separated list of all the datafile locations being analyzed. This should be in the same order as PhenotypeList.
 #' @param ExpectedColumnNames Comma-separ
+
+
+~~~
+> stop("nana", "nana3")
+Error: nananana3
+> stop("nana", " nana3")
+Error: nana nana3
+> val1 <- "shoop"
+> stop("nana", val1, " nana3")
+Error: nanashoop nana3
+> stop("nana ", val1, " nana3")
+Error: nana shoop nana3
+> val2 <- rbind(c("nanana"), c("nanananana"))
+> val2
+     [,1]        
+[1,] "nanana"    
+[2,] "nanananana"
+> stop("nana ", val1, " nana3 ", val2)
+Error: nana shoop nana3 nananananananana
+> print(val2)
+     [,1]        
+[1,] "nanana"    
+[2,] "nanananana"
+> time()
+Error in time.default() : argument "x" is missing, with no default
+> sys.time()
+Error: could not find function "sys.time"
+> Sys.time()
+[1] "2016-08-14 01:11:17 CDT"
+> Sys.time()
+[1] "2016-08-14 01:11:18 CDT"
+> paste(Sys.time(), " nanana"))
+Error: unexpected ')' in "paste(Sys.time(), " nanana"))"
+> paste(Sys.time(), " nanana")
+[1] "2016-08-14 01:11:34  nanana"
+> paste(Sys.time(), "nanana")
+[1] "2016-08-14 01:11:39 nanana"
+> paste(Sys.time(), " -- nanana")
+[1] "2016-08-14 01:11:44  -- nanana"
+> rbind(paste(Sys.time(), " -- nanana"), paste(Sys.time(), " -- nanana2"))
+     [,1]                             
+[1,] "2016-08-14 01:11:59  -- nanana" 
+[2,] "2016-08-14 01:11:59  -- nanana2"
+> rbind(paste(Sys.time(), " -- nanana"), paste(Sys.time(), " -- nanana2"))
+     [,1]                             
+[1,] "2016-08-14 01:12:04  -- nanana" 
+[2,] "2016-08-14 01:12:04  -- nanana2"
+> stop(Sys.time())
+Error: 2016-08-14 01:18:16
+> stop(Sys.time(), " -- blahlbah")
+Error: 2016-08-14 01:18:21 -- blahlbah
+> myfunc <- function(v1) {
++   deparse(substitute(v1))
++ }
+> deparse(substitute(val1))
+[1] "val1"
+> substitute(val1) 
+val1
+> val1
+[1] "shoop"
+> val1 <- c(1,2,3,4,5)
+> val1[c(TRUE,FALSE,FALSE,TRUE,FALSE)]
+[1] 1 4
+> TRUE %in% c(FALSE, FALSE, FALSE)
+[1] FALSE
+> TRUE %in% c(FALSE, FALSE, TRUE)
+[1] TRUE
+> FALSE %in% c(TRUE, TRUE, TRUE)
+[1] FALSE
+> FALSE %in% c(TRUE, TRUE, FALSE)
+[1] TRUE
+
+~~~
+
+
+
+
 
