64261595d178c5299abc4f64e03c1be869c0d8a6
mturchin20 <mturchin20@gmail.com>
2016-09-05 11:28:40 -0500

    20160905 -- Updating files, starting to reorganize .R structure

--- R/PassChecksForDataSources.R
+++ R/PassChecksForDataSources.R
@@ -251,26 +251,8 @@ GetSumAcrossSigmaAlphas_withPriors <- function(LogBFs1, ModelPriors, nGammas, nS
 
 #This is going to be the main function that goes through each of the steps from beginning to end. Hypothetically, all the other functions presented here should be used through the PrepareData process (or as a subfunction of one of the functions being used in PrepareData)
 #PrepareData <- function (ExpectedColumnNames, DataFileLocations, OutputFileBase) { #20160814 NOTE -- Changing direction and just assuming input is a single vector that contains all the proper data.frame datasources and working from there. Final output will be a list that has all the output. 'Logfile' will just be a variable included in final list output, developed by continula 'rbind' calls with text output additions. Also deciding to move 'PrepareData' to just a 'MainWorkFlow' or 'Main' that I'll dev in each sub R package and then eventually move to a main source.  
-bmass <- function (DataSources, GWASsnps=NULL, ExpectedColumnNames=c("Chr", "BP", "MAF", "Direction", "pValue", "N"), SigmaAlphas = c(0.005,0.0075,0.01,0.015,0.02,0.03,0.04,0.05,0.06,0.07,0.08,0.09,0.1,0.15), MergedDataSources=NULL, ProvidedPriors=NULL, UseFlatPriors=FALSE, PruneMarginalHits=TRUE, PruneMarginalHits_bpWindow=5e5, SNPMarginalUnivariateThreshold = 1e-6, SNPMarginalMultivariateThreshold = 1e-6, NminThreshold = 0, bmassSeedValue=NULL) {
-
-	print(DataSources)
-
-	LogFile1 <- c()
-	bmassOutput <- list()
-	bmassOutput$ModelPriors <- NULL
-	bmassOutput$MarginalSNPs <- list() 
-	bmassOutput$MarginalSNPs$SNPs <- NULL
-	bmassOutput$MarginalSNPs$logBFs <- NULL
-	bmassOutput$MarginalSNPs$Posteriors <- NULL
-	bmassOutput$NewSNPs <- list()
-	bmassOutput$NewSNPs$SNPs <- NULL
-	bmassOutput$NewSNPs$logBFs <- NULL
-	bmassOutput$NewSNPs$Posteriors <- NULL
-	bmassOutput$GWASlogBFMinThreshold <- NULL
-	bmassOutput$PreviousSNPs <- list()
-	bmassOutput$PreviousSNPs$SNPs <- NULL
-	bmassOutput$PreviousSNPs$logBFs <- NULL
-	bmassOutput$PreviousSNPs$Posteriors <- NULL
+#bmass <- function (DataSources, GWASsnps=NULL, ExpectedColumnNames=c("Chr", "BP", "MAF", "Direction", "pValue", "N"), SigmaAlphas = c(0.005,0.0075,0.01,0.015,0.02,0.03,0.04,0.05,0.06,0.07,0.08,0.09,0.1,0.15), MergedDataSources=NULL, ProvidedPriors=NULL, UseFlatPriors=FALSE, PruneMarginalHits=TRUE, PruneMarginalHits_bpWindow=5e5, SNPMarginalUnivariateThreshold = 1e-6, SNPMarginalMultivariateThreshold = 1e-6, NminThreshold = 0, bmassSeedValue=NULL) {
+DataChecks <- function (DataSources, GWASsnps, ExpectedColumnNames, SigmaAlphas, MergedDataSources, ProvidedPriors, UseFlatPrior, PruneMarginalHits, PruneMarginalHits_bpWindow, SNPMarginalUnivariateThreshold, SNPMarginalMultivariateThreshold, NminThreshold, bmassSeedValue, LogFile1) {
 
 	LogFile1 <- rbind(LogFile1, paste(format(Sys.time()), " -- beginning bmass.", sep=""))
 
@@ -363,7 +345,7 @@ bmass <- function (DataSources, GWASsnps=NULL, ExpectedColumnNames=c("Chr", "BP"
 		}
 	
 		#20160902 CHECK_0 -- Prob: Go through all other input variables and make sure they are the expected formats/inputs, eg numeric, character, etcetc
-		#bmass <- function (DataSources, GWASsnps=NULL, SNPMarginalpValThreshold=1e-6, ExpectedColumnNames=c("Chr", "BP", "MAF", "Direction", "pValue", "N"), SigmaAlphas = c(0.005,0.0075,0.01,0.015,0.02,0.03,0.04,0.05,0.06,0.07,0.08,0.09,0.1,0.15), MergedDataSources=NULL, ProvidedPriors=NULL, UseFlatPriors=FALSE, PruneMarginalHits=TRUE, PruneMarginalHits_bpWindow=5e5, NminThreshold = 0, bmassSeedValue=NULL) {
+		#bmass <- function (DataSources, GWASsnps=NULL, SNPMarginalpValThreshold=1e-6, ExpectedColumnNames=c("Chr", "BP", "MAF", "Direction", "pValue", "N"), SigmaAlphas = c(0.005,0.0075,0.01,0.015,0.02,0.03,0.04,0.05,0.06,0.07,0.08,0.09,0.1,0.15), MergedDataSources=NULL, ProvidedPriors=NULL, UseFlatPriors=FALSE, PruneMarginalHits=TRUE, PruneMarginalHits_bpWindow=5e5, NminThreshold = 0, bmassSeedValue=NULL) 
 
 		if (!is.numeric(NminThreshold)) {
 			stop(Sys.time(), " -- .")
--- R/bmass.R
+++ R/bmass.R
@@ -19,6 +19,45 @@
 #
 #}
 
+bmass <- function (DataSources, GWASsnps=NULL, ExpectedColumnNames=c("Chr", "BP", "MAF", "Direction", "pValue", "N"), SigmaAlphas = c(0.005,0.0075,0.01,0.015,0.02,0.03,0.04,0.05,0.06,0.07,0.08,0.09,0.1,0.15), MergedDataSources=NULL, ProvidedPriors=NULL, UseFlatPriors=FALSE, PruneMarginalHits=TRUE, PruneMarginalHits_bpWindow=5e5, SNPMarginalUnivariateThreshold = 1e-6, SNPMarginalMultivariateThreshold = 1e-6, NminThreshold = 0, bmassSeedValue=NULL) {
+
+        print(DataSources)
+
+        LogFile1 <- c()
+        bmassOutput <- list()
+        bmassOutput$ModelPriors <- NULL
+        bmassOutput$MarginalSNPs <- list()
+        bmassOutput$MarginalSNPs$SNPs <- NULL
+        bmassOutput$MarginalSNPs$logBFs <- NULL
+        bmassOutput$MarginalSNPs$Posteriors <- NULL
+        bmassOutput$NewSNPs <- list()
+        bmassOutput$NewSNPs$SNPs <- NULL
+        bmassOutput$NewSNPs$logBFs <- NULL
+        bmassOutput$NewSNPs$Posteriors <- NULL
+        bmassOutput$GWASlogBFMinThreshold <- NULL
+        bmassOutput$PreviousSNPs <- list()
+        bmassOutput$PreviousSNPs$SNPs <- NULL
+        bmassOutput$PreviousSNPs$logBFs <- NULL
+        bmassOutput$PreviousSNPs$Posteriors <- NULL
+
+        LogFile1 <- rbind(LogFile1, paste(format(Sys.time()), " -- beginning bmass.", sep=""))
+
+        #20160823 CHECK_0: Prob -- list of Matthew functions specifically to double-check, go through, go over
+        #       collapse
+        #       em.priorprobs
+
+        #Loading and checking data
+        #~~~~~~
+
+	LogFile1 <- DataChecks(DataSources, GWASsnps, ExpectedColumnNames, SigmaAlphas, MergedDataSources, ProvidedPriors, UseFlatPrior, PruneMarginalHits, PruneMarginalHits_bpWindow, SNPMarginalUnivariateThreshold, SNPMarginalMultivariateThreshold, NminThreshold, bmassSeedValue, LogFile1)
+
+}
+
+
+
+
+
+
 
 
 
