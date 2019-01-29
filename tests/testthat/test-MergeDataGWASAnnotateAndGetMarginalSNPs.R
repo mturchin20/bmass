d2ab39fbd4d912f0645714f34b2a22e459e57216
mturchin20 <mturchin20@gmail.com>
2019-01-29 13:35:09 -0600

    Beginning to return to finishing up unit tests

---
+++ tests/testthat/test-MergeDataGWASAnnotateAndGetMarginalSNPs.R
@@ -0,0 +1,39 @@
+context("Tests for MergeDataGWASAnnotateAndGetMarginalSNPs.R")
+
+#library("devtools"); devtools::load_all("/home/mturchin20/project/Lab_Stuff/StephensLab/bmass");
+#bmass_TestData1 <- read.table("bmass_TestData1.txt", header=T)
+#bmass_TestData2 <- read.table("bmass_TestData2.txt", header=T)
+#bmass_TestSigSNPs <- read.table("bmass_TestSigSNPs.txt", header=T)
+
+data(bmass_TestData1, bmass_TestData2, bmass_TestSigSNPs)
+DataSources <- c("bmass_TestData1", "bmass_TestData2")
+ExpectedColumnNames <- c("Chr", "BP", "A1", "MAF", "Direction", "pValue", "N") 
+
+assign("AnnotateTest1", c(1,2,1,0,0,0,0,0,0,0,0), envir = .GlobalEnv)
+assign("AnnotateTest2", c(1,0,1,0,0,0,0,0,0,0,0), envir = .GlobalEnv)
+assign("ZScoreOutput1", c(-7.2004822,-1.7743819,5.9315982,2.7703272,5.3267239,5.4513104,-0.3584588,Inf,0.5100735,-2.1570727,1.6448536), envir = .GlobalEnv)
+assign("ZScoreOutput2", c(-7.3487961,-0.1763742,5.8471721,2.6693421,5.0689577,5.3458374,-1.5547736,-Inf,0.4399132,-1.9514798,1.1263911), envir = .GlobalEnv)
+assign("ZScoreOutput1Replace", c(-7.2004822,-1.7743819,5.9315982,2.7703272,5.3267239,5.4513104,-0.3584588,5.9315982,0.5100735,-2.1570727,1.6448536), envir = .GlobalEnv)
+assign("ZScoreOutput2Replace", c(-7.3487961,-0.1763742,5.8471721,2.6693421,5.0689577,5.3458374,-1.5547736,-7.3487961,0.4399132,-1.9514798,1.1263911), envir = .GlobalEnv)
+
+test_that("AnnotateDataWithGWASSNPs annotates input list of SNPs with a second list of GWAS SNPs", {
+	expect_equal(AnnotateDataWithGWASSNPs(bmass_TestData1, bmass_TestSigSNPs), AnnotateTest1)
+	expect_equal(AnnotateDataWithGWASSNPs(bmass_TestData1, bmass_TestSigSNPs, BPWindow=1), AnnotateTest2)
+})
+
+test_that("GetZScoreAndDirection extracts p-value and direction information and converts to ZScore per SNP", {
+	expect_equal(apply(bmass_TestData1, 1, GetZScoreAndDirection), ZScoreOutput1)
+	expect_equal(apply(bmass_TestData2, 1, GetZScoreAndDirection), ZScoreOutput2)
+})
+
+test_that("CheckForInfiniteZScores checks whether there is an infinite value among the input", {
+	expect_equal(TRUE %in% sapply(ZScoreOutput1, CheckForInfiniteZScores), TRUE)
+	expect_equal(TRUE %in% sapply(ZScoreOutput2[1:5], CheckForInfiniteZScores), FALSE)
+})
+
+test_that("ReplaceInfiniteZScoresWithMax replaces infinite ZScores with the maximum or minimum (as appropriate re: direction of effect) present among other ZScores", {
+	expect_equal(ReplaceInfiniteZScoresWithMax(ZScoreOutput1), ZScoreOutput1Replace)
+	expect_equal(ReplaceInfiniteZScoresWithMax(ZScoreOutput2), ZScoreOutput2Replace)
+})
+
+rm(bmass_TestData1, bmass_TestData2, bmass_TestSigSNPs, AnnotateTest1, AnnotateTest2, ZScoreOutput1, ZScoreOutput2, ZScoreOutput1Replace, ZScoreOutput2Replace, envir = .GlobalEnv)
