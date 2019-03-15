3c502ee99b1f18f21b3322d0e4b47c7fce2390b3
mturchin20 <mturchin20@gmail.com>
2019-03-15 12:59:46 -0500

    Updates related to Peter's suggested revisions

--- R/data.R
+++ R/data.R
@@ -1,11 +1,11 @@
 #' @name bmass_TestData1
 #'
-#' @title bmass Example Dataset 1
+#' @title bmass Test Dataset 1
 #'
 #' @docType data
 #' 
-#' @description A manually created sample dataset for use in vignettes
-#' and unit tests.
+#' @description A manually created sample dataset for use in unit
+#' tests.
 #'
 #' @format A data frame with 11 rows and 9 variables:
 #' 
@@ -30,12 +30,12 @@ NULL
 
 #' @name bmass_TestData2
 #'
-#' @title bmass Example Dataset 2
+#' @title bmass Test Dataset 2
 #'
 #' @docType data
 #'
-#' @description A manually created sample dataset for use in vignettes
-#' and unit tests.
+#' @description A manually created sample dataset for use in unit
+#' tests.
 #'
 #' @format A data frame with 11 rows and 9 variables:
 #' 
@@ -60,12 +60,92 @@ NULL
 
 #' @name bmass_TestSigSNPs
 #' 
-#' @title bmass Example GWAS SNPs
+#' @title bmass Test GWAS SNPs
 #'
 #' @docType data
 #' 
 #' @description A manually created list of GWAS significant SNPs to be
 #' used in conjunction with 'bmass_TestData1' and 'bmass_TestData2'.
+#'
+#' @format A data frame with 2 rows and 2 variables:
+#' 
+#' \describe{
+#'   \item{Chr}{chromosome}
+#'   \item{BP}{basepair position}
+#' }
+#'
+#' @keywords data
+#' 
+#' @source Manually created
+#' 
+NULL
+
+#' @name bmass_SimulatedData1
+#'
+#' @title bmass Simulated Dataset 1
+#'
+#' @docType data
+#' 
+#' @description A manually created sample dataset for use in vignettes.
+#'
+#' @format A data frame with 11 rows and 9 variables:
+#' 
+#' \describe{
+#'   \item{Chr}{chromosome}
+#'   \item{BP}{basepair position}
+#'   \item{Marker}{rsID# or other identifier}
+#'   \item{MAF}{Minor Allele Frequency}
+#'   \item{A1}{reference allele}
+#'   \item{A2}{alternative allele}
+#'   \item{Direction}{direction of association
+#'	effect size, + or -}
+#'   \item{pValue}{p-Value of GWAS association}
+#'   \item{N}{sample size}
+#' }
+#' 
+#' @source Manually created
+#'
+#' @keywords data
+#' 
+NULL
+
+#' @name bmass_SimulatedData2
+#'
+#' @title bmass Simulated Dataset 2
+#'
+#' @docType data
+#'
+#' @description A manually created sample dataset for use in vignettes.
+#'
+#' @format A data frame with 11 rows and 9 variables:
+#' 
+#' \describe{
+#'   \item{Chr}{chromosome}
+#'   \item{BP}{basepair position}
+#'   \item{Marker}{rsID# or other identifier}
+#'   \item{MAF}{Minor Allele Frequency}
+#'   \item{A1}{reference allele}
+#'   \item{A2}{alternative allele}
+#'   \item{Direction}{direction of association
+#'	effect size, + or -}
+#'   \item{pValue}{p-Value of GWAS association}
+#'   \item{N}{sample size}
+#' }
+#'
+#' @keywords data
+#' 
+#' @source Manually created
+#'
+NULL
+
+#' @name bmass_SimulatedSigSNPs
+#' 
+#' @title bmass Simulated GWAS SNPs
+#'
+#' @docType data
+#' 
+#' @description A manually created list of GWAS significant SNPs to be
+#' used in conjunction with 'bmass_SimulatedData1' and 'bmass_SimulatedData2'.
 #'
 #' @format A data frame with 2 rows and 2 variables:
 #' 
@@ -87,7 +167,8 @@ NULL
 #' @docType data
 #' 
 #' @description A list of the univariate GWAS significant SNPs from
-#' the GlobalLipids2013 dataset to be used in the main bmass vignette.
+#' the GlobalLipids2013 dataset to be used in the second introductory 
+#' bmass vignette.
 #'
 #' @format A data frame with 157 rows and 2 variables:
 #' 
